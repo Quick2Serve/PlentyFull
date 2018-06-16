0ad07f0b4a54ed70c6347aff3e54aeb65c4f0520
Nicholas Fordyce <nicholasfordyce@Nicholass-MacBook-Pro.local>
2018-06-16 08:10:22 -0400

    Added taskbar icons

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import { createBottomTabNavigator} from 'react-navigation';
 import {HomeView, myMap, Setting} from './Views/index';
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
 const App = createBottomTabNavigator(
   {
     HomeView:{
       screen: HomeView,
       navigationOptions:{
-        headerTitle: "List",
+        tabBarIcon: ({tintColor}) => <Ionicons name="md-home" size={32} color={tintColor}/>,
       },
     },
     myMap:{
       screen: myMap,
       navigationOptions:{
-        headerTitle: "Map",
+        tabBarIcon: ({tintColor}) => <Ionicons name="md-map" size={32} color={tintColor}/>,
       },
     },
     Setting:{
       screen: Setting,
       navigationOptions:{
         headerTitle: "Setting",
+        tabBarIcon: ({tintColor}) => <Ionicons name="md-settings" size={32} color={tintColor}/>,
       },
     }
   }
