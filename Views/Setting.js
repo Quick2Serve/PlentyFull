6da4fc850a5300bcf2dec8d626019c4bce29fcfb
Nicholas Fordyce <nicholasfordyce@Nicholass-MacBook-Pro.local>
2018-06-16 15:58:09 -0400

    finished settings page

--- Views/Setting.js
+++ Views/Setting.js
@@ -1,48 +1,56 @@
 import React from 'react';
-import { View } from 'react-native';
-import { CheckBox, SearchBar, List, ListItem, Text, Header } from 'react-native-elements'
+import { View, ScrollView } from 'react-native';
+import { Card, CheckBox, SearchBar, List, ListItem, Text, Header } from 'react-native-elements'
 
 const list = [
-  {
-    name: 'Mary Rigg Neighborhood Center',
-  },
-  {
-    name: 'Mid-North Food Pantry',
-  },
+ {
+   name: 'Mary Rigg Neighborhood Center',
+ },
+ {
+   name: 'Mid-North Food Pantry',
+ },
 ]
 
 export default class Setting extends React.Component {
-  render(){
-    return (
-      <View style={{flex: 1}}>
-      <Header
-        centerComponent={{ text: 'Plentyfull', style: { color: '#fff' } }}
-      />
-      <View style={{ flex: 1, justifyContent: 'center' }}>
-        <Text h2>Settings</Text>
-        <Text h4>Filters Organizations By</Text>
-        <CheckBox title="Distance" checked={true} checkedIcon='dot-circle-o' uncheckedIcon='circle-o' checkedColor='blue'/>
-        <CheckBox title="Time" checked={false} checkedIcon='dot-circle-o' uncheckedIcon='circle-o' checkedColor='blue'/>
-        <Text h4>Options</Text>
-        <CheckBox title="Enable Push Notifications" checked={true} checkedColor='blue'/>
-        <CheckBox title="Night Mode" checked={false} checkedColor='blue'/>
-        <Text h4>Blacklist Organizations</Text>
-        <SearchBar
-          lightTheme
-          placeholder='Type Here...' />
-        <List containerStyle={{marginBottom: 20}}>
-        {
-          list.map((l, i) => (
-            <ListItem
-              hideChevron={true}
-              key={i}
-              title={l.name}
-            />
-          ))
-        }
-      </List>
-      </View>
-      </View>
-    );
-  }
+ render(){
+   return (
+     <View style={{flex: 1, marginBottom: 20}}>
+     <Header
+       centerComponent={{ text: 'Setting', style: { color: '#fff' } }}
+     />
+     <ScrollView>
+       <View style={{ flex: 1}}>
+         <Text style={{fontSize: 15, paddingLeft: 10, marginTop: 10}}>Filters Organizations By</Text>
+         <Card loading={false}>
+           <CheckBox title="Distance" checked={true} checkedIcon='dot-circle-o' uncheckedIcon='circle-o' checkedColor='blue'/>
+           <CheckBox title="Time" checked={false} checkedIcon='dot-circle-o' uncheckedIcon='circle-o' checkedColor='blue'/>
+         </Card>
+         <Text style={{fontSize: 15, paddingLeft: 10, marginTop: 10}}>Options</Text>
+         <Card loading={false}>
+           <CheckBox title="Enable Push Notifications" checked={true} checkedColor='blue'/>
+           <CheckBox title="Night Mode" checked={false} checkedColor='blue'/>
+         </Card>
+           <Text style={{fontSize: 15, paddingLeft: 10, marginTop: 10}}>Blacklist Organizations</Text>
+         <Card loading={false}>
+           <SearchBar
+             lightTheme
+             placeholder='Type Here...' />
+           <List containerStyle={{marginBottom: 20}}>
+         {
+           list.map((l, i) => (
+             <ListItem
+               hideChevron={true}
+               key={i}
+               title={l.name}
+             />
+           ))
+         }
+       </List>
+       </Card>
+       </View>
+     </ScrollView>
+     </View>
+   );
+ }
 }
+
