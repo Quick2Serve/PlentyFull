a01889d97f48816fe9090c163499815b59c3ab0c
William Kang <kangwill4@gmail.com>
2018-06-15 21:35:04 -0400

    Added navbar to app

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavagator} from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+const App = TabNaviator(
+  {
+    ListView:{
+      screen: ListView,
+      navigationOptions:{
+        headerTitle: "List",
+      },
+    }
+    MapView:{
+      screen: mapView,
+      navigationOptions:{
+        headerTitle: "Map",
+      },
+    }
+    Setting{
+      screen: setting,
+      navigationOptions:{
+        headerTitle: "Setting",
+      },
+    }
   }
-}
+)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
